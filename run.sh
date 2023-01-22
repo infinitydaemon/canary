c16079df0def455b2e6e9846e8f00d790a1de165
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-01-22 09:42:32 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ CONF="/etc/opencanaryd/opencanary.conf"
 
 if [ -f $CONF ]; then
 	echo "INFO: Main configuration file found"
-	env/bin/opencanaryd --start
+	/home/cwd/env/bin/opencanaryd --start
 else
 	opencanaryd --copyconfig && echo "A Config file was generated at /etc/opencanaryd/.opencanary.conf."
 	echo "INFO: Re-run the script to start the canary daemon"
