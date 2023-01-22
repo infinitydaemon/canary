efb840ab590f89a3a18dca95f8dc1840a3333dd7
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-01-22 09:41:37 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
 CONF="/etc/opencanaryd/opencanary.conf"
-TEMP_CONF="/etc/opencanaryd/.opencanary.conf"
 
 if [ -f $CONF ]; then
 	echo "INFO: Main configuration file found"
-	opencanaryd --start
-elif [ -f $TEMP_CONF ]; then
-	echo "INFO: Temp configuration file found"
-	opencanaryd --dev
+	env/bin/opencanaryd --start
 else
 	opencanaryd --copyconfig && echo "A Config file was generated at /etc/opencanaryd/.opencanary.conf."
+	echo "INFO: Re-run the script to start the canary daemon"
 fi
 
