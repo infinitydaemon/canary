315b403a21cc5ea09fddf51bcee1c5c01710c4bc
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 14:21:08 +0500

    Update CanaryToken_UploadtoS3.py

--- utils/python/CanaryToken_UploadtoS3.py
+++ utils/python/CanaryToken_UploadtoS3.py
@@ -1,8 +1,8 @@
-#CanaryToken_UploadtoS3.py
-#Make sure AWS Boto3 is installed with "pip install boto3"
-#Make sure the requests package is installed with "pip install requests"
+# CanaryToken_UploadtoS3.py
+# Make sure AWS Boto3 is installed with "pip install boto3"
+# Make sure the requests package is installed with "pip install requests"
 
-#Imports
+# Imports
 import requests
 import os
 import re
@@ -11,47 +11,47 @@ import json
 import boto3
 from botocore.exceptions import NoCredentialsError
 
-#Variables
+# Variables
 
-DOMAIN = 'XXXXXX.canary.tools' # Enter your Console domain between the . e.g. 1234abc.canary.tools
-FACTORYAUTH = 'XXXXX' # Enter your Factory auth key. e.g a1bc3e769fg832hij3 Docs available here. https://docs.canary.tools/canarytokens/factory.html#create-canarytoken-factory-auth-string
-MEMO = socket.gethostname()+' S3 Token Upload'
-TOKENTYPE = 'aws-id' # Enter your required token type. Full list available here. https://docs.canary.tools/canarytokens/factory.html#list-canarytokens-available-via-canarytoken-factory
-ACCESS_KEY = 'AKIAZ3PZQS2BK6GXM446'
-SECRET_KEY = 'YD5ZNCXRrZcWclBObUHw3U24o/WlC3UGLIuuBWk9'
+DOMAIN = 'XXXXXX.canary.tools'  # Enter your Console domain between the . e.g. 1234abc.canary.tools
+FACTORYAUTH = 'XXXXX'  # Enter your Factory auth key. e.g a1bc3e769fg832hij3 Docs available here. https://docs.canary.tools/canarytokens/factory.html#create-canarytoken-factory-auth-string
+MEMO = socket.gethostname() + ' S3 Token Upload'
+TOKENTYPE = 'aws-id'  # Enter your required token type. Full list available here. https://docs.canary.tools/canarytokens/factory.html#list-canarytokens-available-via-canarytoken-factory
+ACCESS_KEY = 'YOUR_ACCESS_KEY'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
-#Create token on console
+# Create token on console
 
-create_url = 'https://'+DOMAIN+'/api/v1/canarytoken/factory/create'
+create_url = 'https://' + DOMAIN + '/api/v1/canarytoken/factory/create'
 
 payload = {
-  'factory_auth': FACTORYAUTH,
-  'memo': MEMO,
-  'kind': TOKENTYPE
+    'factory_auth': FACTORYAUTH,
+    'memo': MEMO,
+    'kind': TOKENTYPE
 }
 
-Tokencreation = requests.post(create_url, data=payload)
-Tokencreation_result = Tokencreation.json()
-TokenID = Tokencreation_result["canarytoken"]["canarytoken"]
+token_creation = requests.post(create_url, data=payload)
+token_creation_result = token_creation.json()
+token_id = token_creation_result["canarytoken"]["canarytoken"]
 
-#download token locally
+# Download token locally
 
-download_url = 'https://'+DOMAIN+'/api/v1/canarytoken/factory/download'
+download_url = 'https://' + DOMAIN + '/api/v1/canarytoken/factory/download'
 
 payload = {
-  'factory_auth': FACTORYAUTH,
-  'canarytoken': TokenID
+    'factory_auth': FACTORYAUTH,
+    'canarytoken': token_id
 }
 
 download_request = requests.get(download_url, allow_redirects=True, params=payload)
 filename = re.findall("filename=(.+)", download_request.headers["Content-Disposition"])[0]
 with open(filename, 'wb') as f:
-  f.write(download_request.content)
+    f.write(download_request.content)
 
-#upload token to S3
+# Upload token to S3
 
 def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, )
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
     try:
         s3.upload_file(local_file, bucket, s3_file)
         print("Upload Successful")
@@ -63,12 +63,12 @@ def upload_to_aws(local_file, bucket, s3_file):
         print("Credentials not available")
         return False
 
-uploaded = upload_to_aws('/Users/gareth/credentials.txt', 'tokenuploadtest07092021', 'credentials.txt')
- 
-#clean up local token.
+uploaded = upload_to_aws(filename, 'YOUR_BUCKET_NAME', 'YOUR_FILE_NAME')
 
-os.remove("demofile.txt")
+# Clean up local token.
 
-#user feedback.
+os.remove(filename)
+
+# User feedback.
 
 print("Token Upload to S3 complete")
