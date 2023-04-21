2f44ea4d06d5961e73abb08cef1d32b8b71b4b94
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 13:38:59 +0500

    Update honeycred.py

--- opencanary/honeycred.py
+++ opencanary/honeycred.py
@@ -1,19 +1,43 @@
 import functools
 from passlib.context import CryptContext
 
-__all__ = ["buildHoneyCredHook", "cryptcontext"]
-
-cryptcontext = CryptContext(schemes=["pbkdf2_sha512","bcrypt", "sha512_crypt", "plaintext"])
+cryptcontext = CryptContext(schemes=["pbkdf2_sha512", "bcrypt", "sha512_crypt", "plaintext"])
 
 def buildHoneyCredHook(creds):
+    """
+    Return a callable object that can be used as a honeycred_hook in
+    OpenCanary's configuration file.
+
+    The callable object will check if a given username and password match
+    any of the credentials specified in `creds`.
+
+    Args:
+        creds (list of dicts): List of credentials to match against.
+
+    Returns:
+        Callable: A function that takes a username and password as arguments
+            and returns True if they match any of the credentials in `creds`.
+
+    """
     return functools.partial(testManyCreds, creds)
 
 def testCred(cred, username=None, password=None):
     """
-    Test if given credentials matches specified credentials
+    Test if given credentials match specified credentials.
 
-    If specified credentials doesn't have username or password, it
+    If specified credentials do not have a username or password, it
     will still match on the other.
+
+    Args:
+        cred (dict): A dictionary representing the credentials to match against.
+            It can have the following keys:
+                - username: The username to match (optional).
+                - password: The password hash to match (optional).
+        username (bytes): The username to test (optional).
+        password (bytes): The password to test (optional).
+
+    Returns:
+        bool: True if the given username and password match the specified credentials.
 
     """
     cred_username = cred.get("username", None)
@@ -30,6 +54,18 @@ def testCred(cred, username=None, password=None):
     return (user_match and password_match)
 
 def testManyCreds(creds, username=None, password=None):
+    """
+    Test if given credentials match any of the specified credentials.
+
+    Args:
+        creds (list of dicts): List of credentials to match against.
+        username (bytes): The username to test (optional).
+        password (bytes): The password to test (optional).
+
+    Returns:
+        bool: True if the given username and password match any of the credentials.
+
+    """
     for c in creds:
         if testCred(c, username, password):
             return True
