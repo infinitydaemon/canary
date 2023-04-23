620e04bea32a50594e3e93493b6f0ad77aa10528
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-23 10:19:10 +0500

    Update delete_tokens.py
    * Change main function to delete_all_canary_tokens for clarity.
    * Changed the if statement checking for the correct number of arguments to a sys.exit(1) call to exit the program with a non-zero exit code if the correct arguments are not provided.
    * Removed the unnecessary re import.

--- utils/python/delete_tokens.py
+++ utils/python/delete_tokens.py
@@ -1,7 +1,7 @@
 # Delete Tokens
 # delete_tokens.py
-# Authors: Jay wrote most of this and Adrian updated to Python3
-#
+# Authors: Initially written by Jay then updated Adrian updated to Python3. Imroved and updated by  CWD SYSTEMS
+#        
 # Be VERY careful with this script! It is designed to wipe out all tokens 
 # after some heavy automated API testing. Don't use it if you have any
 # production tokens that you've worked hard to create and deploy - you'll
@@ -10,38 +10,46 @@
 
 import requests
 import sys
-import re
 
-def main(args):
-    if len(args) < 2:
-        print("usage: python delete_tokens.py <console_url> <api_key>")
-    auth = args[1]
-    console = args[0]
-    get_url = "{base}/api/v1/canarytokens/fetch?auth_token={auth}".format(
-        base=console, auth=auth)
+def delete_all_canary_tokens(console_url, api_key):
+    get_url = f"{console_url}/api/v1/canarytokens/fetch?auth_token={api_key}"
     resp = requests.get(get_url)
-    resp_obj =  resp.json()
+    resp_obj = resp.json()
+    
     print("Current tokens on your console")
     print("-----------------------------------------------------------------------------------")
     print("kind\t\ttoken\t\t\t\t\tmemo")
     print("-----------------------------------------------------------------------------------")
+    
     for token in resp_obj['tokens']:
         print("{}\t\t{}\t\t{}".format(token['kind'], token['canarytoken'], token['memo']))
+        
     print("-----------------------------------------------------------------------------------")
-    canarytoken = input("Are you sure you would like to delete all your Canarytokens? [Y\\n] PLEASE NOTE: This is irreversible!  ")
-    if canarytoken != 'Y':
+    
+    user_input = input("Are you sure you would like to delete all your Canarytokens? [Y\\n] PLEASE NOTE: This is irreversible!  ")
+    
+    if user_input != 'Y':
         print("Not deleting any canarytokens from your Canary console.")
-        exit(0)
-    delete_url = "{base}/api/v1/canarytoken/delete".format(base=console)
+        return
+    
+    delete_url = f"{console_url}/api/v1/canarytoken/delete"
+    
     for token in resp_obj['tokens']:
-        print("Deleting {}: {}".format(token['canarytoken'], token['kind']))
+        print(f"Deleting {token['canarytoken']}: {token['kind']}")
         data = {
-            'auth_token': auth,
+            'auth_token': api_key,
             'canarytoken': token['canarytoken']
         }
         resp = requests.post(delete_url, data=data)
+        
     print("-----------------------------------------------------------------------------------")
     print("All deleted. Go create some more! They're on us ;)")
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) < 3:
+        print("usage: python delete_tokens.py <console_url> <api_key>")
+        sys.exit(1)
+
+    console_url = sys.argv[1]
+    api_key = sys.argv[2]
+    delete_all_canary_tokens(console_url, api_key)
