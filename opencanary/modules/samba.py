3829f965acbf06827f02d6cb9b8ec699ff0b1a60
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-10-19 13:56:47 +0600

    Update samba.py

--- opencanary/modules/samba.py
+++ opencanary/modules/samba.py
@@ -1,71 +1,72 @@
 import sys
+from opencanary.modules import CanaryService, FileSystemWatcher
+from opencanary.config import ConfigException
 
-if sys.platform.startswith("linux"):
-    from opencanary.modules import CanaryService, FileSystemWatcher
-    from opencanary.config import ConfigException
+class SambaLogWatcher(FileSystemWatcher):
+    def __init__(self, logFile=None, logger=None):
+        super().__init__(fileName=logFile)
+        self.logger = logger
 
-    import os, re, random, shutil, time
-    from datetime import datetime
+    def handleLines(self, lines=None):
+        audit_re = re.compile(r'^.*smbd_audit:.*$')
 
-    class SambaLogWatcher(FileSystemWatcher):
+        for line in lines:
+            matches = audit_re.match(line)
 
-        def __init__(self, logFile=None, logger=None):
-            self.logger = logger
-            FileSystemWatcher.__init__(self, fileName=logFile)
+            if matches is None:
+                continue
 
-        def handleLines(self, lines=None):
-            audit_re = re.compile(r'^.*smbd_audit:.*$')
+            data = self.extract_smb_data(line)
+            self.log_smb_data(data)
 
-            for line in lines:
-                    matches = audit_re.match(line)
+    def extract_smb_data(self, line):
+        data = {}
+        parts = line.split('smbd_audit:', 1)[-1].strip().split('|')
 
-                    #Skip lines that do not match the correct RegEx pattern
-                    if matches is None:
-                           continue
+        data['USER'] = parts[0] or "anonymous"
+        data['SRC_HOST'] = parts[1]
+        data['DST_HOST'] = parts[2]
+        data['SRC_HOST_NAME'] = parts[3]
+        data['SHARE_NAME'] = parts[4]
+        data['DST_HOST_NAME'] = parts[5]
+        data['SMB_VERSION'] = parts[6]
+        data['SMB_ARCH'] = parts[7]
+        data['DOMAIN_NAME'] = parts[9]
+        data['AUDIT_ACTION'] = parts[10]
+        data['AUDIT_STATUS'] = parts[11]
+        data['PATH'] = parts[12]
 
-                    data = line.split('smbd_audit:',1)[-1].strip().split('|')
+        return data
 
-                    user = data[0]
-                    srcHost = data[1]
-                    dstHost = data[2]
-                    srcHostName = data[3]
-                    shareName = data[4]
-                    dstHostName = data[5]
-                    smbVersion = data[6]
-                    smbArch = data[7]
-                    domainName = data[9]
-                    auditAction = data[10]
-                    auditStatus = data[11]
-                    path = data[12]
+    def log_smb_data(self, data):
+        log_data = {
+            'src_host': data['SRC_HOST'],
+            'src_port': '-1',
+            'dst_host': data['DST_HOST'],
+            'dst_port': 445,
+            'logtype': self.logger.LOG_SMB_FILE_OPEN,
+            'logdata': {
+                'USER': data['USER'],
+                'REMOTENAME': data['SRC_HOST_NAME'],
+                'SHARENAME': data['SHARE_NAME'],
+                'LOCALNAME': data['DST_HOST_NAME'],
+                'SMBVER': data['SMB_VERSION'],
+                'SMBARCH': data['SMB_ARCH'],
+                'DOMAIN': data['DOMAIN_NAME'],
+                'AUDITACTION': data['AUDIT_ACTION'],
+                'STATUS': data['AUDIT_STATUS'],
+                'FILENAME': data['PATH']
+            }
+        }
+        self.logger.log(log_data)
 
-                    if user == "":
-                      user = "anonymous"
+class CanarySamba(CanaryService):
+    NAME = 'smb'
 
-                    data = {}
-                    data['src_host'] = srcHost
-                    data['src_port'] = '-1'
-                    data['dst_host'] = dstHost
-                    data['dst_port'] = 445
-                    data['logtype'] =  self.logger.LOG_SMB_FILE_OPEN
-                    data['logdata'] = {'USER':user, 'REMOTENAME': srcHostName, 'SHARENAME': shareName,
-                                       'LOCALNAME': dstHostName, 'SMBVER': smbVersion, 'SMBARCH': smbArch,
-                                       'DOMAIN': domainName, 'AUDITACTION': auditAction,
-                                       'STATUS':auditStatus, 'FILENAME': path}
-                    self.logger.log(data)
+    def __init__(self, config=None, logger=None):
+        super().__init__(config=config, logger=logger)
+        self.audit_file = config.getVal('smb.auditfile', default='/var/log/samba-audit.log')
 
-    class CanarySamba(CanaryService):
-        NAME = 'smb'
-        def __init__(self,config=None, logger=None):
-            CanaryService.__init__(self, config=config, logger=logger)
-            self.audit_file = config.getVal('smb.auditfile', default='/var/log/samba-audit.log')
-            self.config = config
-
-        def startYourEngines(self, reactor=None):
-            #create samba run dir, so testparm doesn't error
-            #try:
-            #    os.stat('/var/run/samba')
-            #except OSError:
-            #    os.mkdir('/var/run/samba')
-
-            fs = SambaLogWatcher(logFile=self.audit_file, logger=self.logger)
-            fs.start()
+    def startYourEngines(self, reactor=None):
+        fs = SambaLogWatcher(logFile=self.audit_file, logger=self.logger)
+        fs.start()
