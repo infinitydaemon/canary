10c7eca5ae28005e52e57cd8c29c8117a036f365
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2024-04-10 21:43:38 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 import codecs
 import os.path
 from setuptools import setup, find_namespace_packages
-import sys
-
 
 def read(rel_path):
     # Get the absolute path of the file relative to the current directory
     here = os.path.abspath(os.path.dirname(__file__))
     # Read the contents of the file using the codecs module to handle Unicode
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), 'r', encoding='utf-8') as fp:
         return fp.read()
 
 def get_version(rel_path):
@@ -24,21 +22,19 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 requirements = [
-    'Twisted==19.10.0',
-    'pyasn1==0.4.5',
-    'cryptography==3.0',
-    'simplejson==3.16.0',
-    'requests==2.21.0',
-    'zope.interface==5.0.0',
+    'Twisted==21.7.0',
+    'pyasn1==0.4.8',
+    'cryptography==3.4.8',
+    'simplejson==3.17.3',
+    'requests==2.26.0',
+    'zope.interface==5.4.0',
     'PyPDF2==1.26.0',
     'fpdf==1.7.2',
-    'passlib==1.7.1',
+    'passlib==1.7.4',
     'Jinja2==3.0.1',
-    'ntlmlib==0.72',
-    'bcrypt==3.1.7',
-    'setuptools==63.2.0',
-    'hpfeeds==3.0.0']
-
+    'ntlmlib==0.92',
+    'bcrypt==3.2.0',
+]
 
 setup(
     name='opencanary',
@@ -47,7 +43,8 @@ setup(
     author='Thinkst Applied Research',
     author_email='info@thinkst.com',
     description='OpenCanary daemon',
-    long_description='A low interaction honeypot intended to be run on internal networks.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     install_requires=requirements,
     license='BSD',
     packages=find_namespace_packages(
@@ -56,7 +53,7 @@ setup(
             'opencanary.test','opencanary.test*'
         ]
     ),
-    include_package_data = True,
+    include_package_data=True,
     scripts=['bin/opencanaryd','bin/opencanary.tac'],
     platforms='any',
     classifiers=[
@@ -71,9 +68,9 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Operating System :: POSIX :: BSD :: FreeBSD",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: BSD License",
     ],
 )
