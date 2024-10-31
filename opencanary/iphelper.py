797cae22d74f445a247aed455be610b7336af15a
cwd.systems | 0KN <101006007+infinitydaemon@users.noreply.github.com>
2024-10-31 19:57:30 +0600

    Update iphelper.py

--- opencanary/iphelper.py
+++ opencanary/iphelper.py
@@ -1,5 +1,3 @@
-import struct
-import socket
 import ipaddress
 
 def ip2int(addr: str) -> int:
@@ -12,15 +10,14 @@ def ip2int(addr: str) -> int:
     Returns:
         int: The IP address in integer format.
     """
-    return struct.unpack("!I", socket.inet_aton(addr))[0]
+    return int(ipaddress.ip_address(addr))
 
 def check_ip(ip: str, network_range: str) -> bool:
     """
     Test if an IP address is in a given network range.
 
-    The network range is expected to be in CIDR notation format. If no subnet
-    mask is given, /32 is used. Returns True if the IP is in the range, False
-    otherwise.
+    The network range is expected to be in CIDR notation format.
+    Returns True if the IP is in the range, False otherwise.
 
     Args:
         ip (str): The IP address to check.
@@ -29,14 +26,11 @@ def check_ip(ip: str, network_range: str) -> bool:
     Returns:
         bool: True if the IP is in the range, False otherwise.
     """
-    network_addr, subnet_mask = network_range.split('/')
-    subnet_mask = int(subnet_mask) if subnet_mask else 32
     try:
-        network_addr_int = ip2int(network_addr)
-        ip_int = ip2int(ip)
-        subnet_mask_int = (0xFFFFFFFF << (32 - subnet_mask)) & 0xFFFFFFFF
-        return (ip_int & subnet_mask_int) == (network_addr_int & subnet_mask_int)
-    except (socket.error, struct.error):
+        ip_obj = ipaddress.ip_address(ip)
+        network = ipaddress.ip_network(network_range, strict=False)
+        return ip_obj in network
+    except ValueError:
         return False
 
 # Example usage:
